fe0a541fca70643dbd443e6f468aee08969f8c3a
anjum Katiyar <anjum.kaiyar@bhn.com>
2026-02-20 11:07:36 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/devops/learning/DevopsLearningAppApplication.java
@@ -0,0 +1,13 @@
+package com.devops.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsLearningAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsLearningAppApplication.class, args);
+    }
+}
+
---
+++ src/main/java/com/devops/learning/HelloController.java
@@ -0,0 +1,14 @@
+package com.devops.learning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello! DevOps learning project is running.";
+    }
+}
+
