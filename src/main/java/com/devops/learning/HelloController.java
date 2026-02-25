6e690df6c3de6bfc6c3eb42201499baae4b28d4b
anjum Katiyar <anjum.kaiyar@bhn.com>
2026-02-25 15:26:44 +0530

    Added HomeController for root mapping

--- src/main/java/com/devops/learning/HelloController.java
+++ src/main/java/com/devops/learning/HelloController.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String sayHello() {
         return "Hello! DevOps learning project is running.";
     }
