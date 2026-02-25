41c5c83257cf467cdc97163957de66719faf53c5
anjum Katiyar <anjum.kaiyar@bhn.com>
2026-02-25 15:03:32 +0530

    delete some line in HelloController.java

--- src/main/java/com/devops/learning/HelloController.java
+++ src/main/java/com/devops/learning/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello! DevOps learning project is running.";
